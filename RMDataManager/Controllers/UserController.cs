9cfef7a27b4ba3c93602463bde454ae1d5f96528
YasserElmor <elmor.yasser@gmail.com>
2022-10-20 02:39:18 +0200

    Added a UserController ApiController.
    Adds a new api endpoint (GET api/User) which can be used to fetch the logged-in user's data.
    
    Adds a new connection string for the RMData DB besides the DefaultConnection established for Identity's DB.

---
+++ RMDataManager/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Identity;
+using RMDataManager.Library.DataAccess;
+using RMDataManager.Library.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace RMDataManager.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        // GET api/User
+        public UserModel GetById()
+        {
+            // TODO: Disconnect this dependency using DI
+            UserData data = new UserData();
+
+            string userId = RequestContext.Principal.Identity.GetUserId();
+
+            UserModel userData = data.GetUserById(userId).First();
+
+            return userData;
+        }
+    }
+}
