a28f289c00ff875cf028fe74699f03d6cef364e0
YasserElmor <elmor.yasser@gmail.com>
2022-10-20 02:48:39 +0200

    Add RMDataManager.Library as backend class library.
    Adds a new class library project to provide data access to various backend APIs.

---
+++ RMDataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,21 @@
+﻿using RMDataManager.Library.Internal.DataAccess;
+using RMDataManager.Library.Models;
+using System.Collections.Generic;
+
+namespace RMDataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public List<UserModel> GetUserById(string Id)
+        {
+            // TODO: set up AutoFac for DI, and code against interfaces instead of concrete classes
+            SqlDataAccess sql = new SqlDataAccess();
+
+            var p = new { Id };
+
+            List<UserModel> data = sql.LoadData<UserModel, dynamic>("dbo.spUserLookup", p, "RMData");
+
+            return data;
+        }
+    }
+}
