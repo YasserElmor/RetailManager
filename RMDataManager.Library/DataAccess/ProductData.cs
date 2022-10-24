bf8a7faff9cdcb5a34ac9cb6bbb07fd9d8cd45fe
YasserElmor <elmor.yasser@gmail.com>
2022-10-24 22:51:48 +0200

    Add Data Access for Products
    Adds a Data-Access-Layer's Product Model.
    Adds a Data-Access class (ProductData) for using Product-related stored procedures in order to provide abstract access to the DB for the APIs.

---
+++ RMDataManager.Library/DataAccess/ProductData.cs
@@ -0,0 +1,19 @@
+﻿using RMDataManager.Library.Internal.DataAccess;
+using RMDataManager.Library.Models;
+using System.Collections.Generic;
+
+namespace RMDataManager.Library.DataAccess
+{
+    public class ProductData
+    {
+        public List<ProductModel> GetProducts()
+        {
+                // TODO: set up AutoFac for DI, and code against interfaces instead of concrete classes
+                SqlDataAccess sql = new SqlDataAccess();
+
+                List<ProductModel> data = sql.LoadData<ProductModel, dynamic>("dbo.spProductGetAll", new { }, "RMData");
+
+                return data;
+        }
+    }
+}
