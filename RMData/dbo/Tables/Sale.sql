5c33097deb079b699ba936fbf428d1ede3722a1b
YasserElmor <elmor.yasser@gmail.com>
2022-11-01 04:45:46 +0200

    Add FK Constraints to DB tables.

--- RMData/dbo/Tables/Sale.sql
+++ RMData/dbo/Tables/Sale.sql
@@ -3,8 +3,10 @@
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
 	[CashierId] NVARCHAR(128) NOT NULL, /* Reference to the User Id which references the Auth Id */ 
 	[SaleDate] DATETIME2(7) NOT NULL, 
-    [SubTotal] MONEY NOT NULL, 
-    [Tax] MONEY NOT NULL, /* this is the overall tax value for the sale, but different items have different taxable values that changes over time */
+	[SubTotal] MONEY NOT NULL, 
+	[Tax] MONEY NOT NULL, /* this is the overall tax value for the sale, but different items have different taxable values that changes over time */
 						  /* for that we'll add a Tax record specific to each item in the SaleDetail table */
-    [Total] MONEY NOT NULL,
+	[Total] MONEY NOT NULL, 
+	CONSTRAINT [FK_Sale_ToUser] FOREIGN KEY (CashierId) REFERENCES [User](Id),
+
 )
--- RMData/dbo/Tables/SaleDetail.sql
+++ RMData/dbo/Tables/SaleDetail.sql
@@ -5,5 +5,7 @@
     [ProductId] INT NOT NULL, 
     [Quantity] INT NOT NULL DEFAULT 1,
     [PurchasePrice] MONEY NOT NULL, /* this is used alongside the default product selling RetailPrice in case we wanted to sell it for a different price (discounts, exceptions, etc.) */
-    [Tax] MONEY NOT NULL DEFAULT 0,
+    [Tax] MONEY NOT NULL DEFAULT 0, 
+    CONSTRAINT [FK_SaleDetail_ToSale] FOREIGN KEY (SaleId) REFERENCES Sale(Id), 
+    CONSTRAINT [FK_SaleDetail_ToProduct] FOREIGN KEY (ProductId) REFERENCES Product(Id),
 )
