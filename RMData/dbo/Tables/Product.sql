07cfd9d48d40485f4c450e371e67201d03c53ac4
YasserElmor <elmor.yasser@gmail.com>
2022-11-01 03:28:20 +0200

    Add IsTaxable Flag to Product
    Adds a boolean flag to dictate whether a given product is taxable or not. It's added to the Products table model, the backend's data access ProductModel, and the UI's display ProductModel.

--- RMData/dbo/Tables/Product.sql
+++ RMData/dbo/Tables/Product.sql
@@ -1,10 +1,11 @@
 ﻿CREATE TABLE [dbo].[Product]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [ProductName] NVARCHAR(100) NOT NULL, 
-    [Description] NVARCHAR(MAX) NOT NULL, 
-    [RetailPrice] MONEY NOT NULL, /* Default Selling Price */
-    [QuantityInStock] int NOT NULL DEFAULT 1, /* Inventory Quantity minus sold units in the SaleDetail aggregate */
-    [CreatedDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
-    [LastModified] DATETIME2 NOT NULL DEFAULT getutcdate()
+	[ProductName] NVARCHAR(100) NOT NULL, 
+	[Description] NVARCHAR(MAX) NOT NULL, 
+	[RetailPrice] MONEY NOT NULL, /* Default Selling Price */
+	[QuantityInStock] int NOT NULL DEFAULT 1, /* Inventory Quantity minus sold units in the SaleDetail aggregate */
+	[IsTaxable] bit NOT NULL DEFAULT 1,
+	[CreatedDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
+	[LastModified] DATETIME2 NOT NULL DEFAULT getutcdate()
 )
---
+++ RMDesktopUI.Library/Helpers/ConfigHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+
+namespace RMDesktopUI.Library.Helpers
+{
+    // The ConfigHelper class is used to read data from the UI's App.ccnfig's <appSettings>
+    // and would be registered as a singleton for the sole purpose of accessing this data
+    public class ConfigHelper : IConfigHelper
+    {
+        public decimal GetTaxRate()
+        {
+            string rateText = ConfigurationManager.AppSettings["taxRate"];
+
+            bool IsValidTaxRate = decimal.TryParse(rateText, out decimal output);
+
+            if (!IsValidTaxRate)
+                throw new ConfigurationErrorsException("The taxRate is not set up properly");
+
+            return output;
+        }
+
+    }
+}
